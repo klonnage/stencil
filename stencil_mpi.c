f51037174627950cd2506148ec65752f940e1904
Bridonneau Vincent <vincent.bridonneau@enseirb-matmeca.fr>
2020-01-24 13:14:58 +0100

    Corners communications working, convergence step OK

--- stencil_mpi.c
+++ stencil_mpi.c
@@ -6,8 +6,8 @@
 #include <getopt.h>
 #include <mpi.h>
 
-#define STENCIL_SIZE_X 10
-#define STENCIL_SIZE_Y 10
+#define STENCIL_SIZE_X 5
+#define STENCIL_SIZE_Y 5
 
 /** number of buffers for N-buffering; should be at least 2 */
 #define STENCIL_NBUFFERS 2
@@ -229,26 +229,6 @@ static void stencil_step(void)
 
 /* Send and receive data to all neighbors */
 void stencil_update() {
-  // values[current_buffer][index_send[d][0] + 2][index_send[d][1]] = 3.;
-
-  /*MPI_Datatype type = comm_types[d];
-  MPI_Sendrecv(&values[current_buffer][index_send[d][0]][index_send[d][1]], 1, row, neighbors[d], d,
-               buff, STENCIL_SIZE_X-2, MPI_DOUBLE, neighbors[opposite[d]], d, grid, MPI_STATUS_IGNORE);*/
-  
-  /*my_send_recv_directions(&values[current_buffer][index_send[d][0]][index_send[d][1]],
-                          &values[current_buffer][index_recv[d][0]][index_recv[d][1]], d);
-  for (int i = 0; i < STENCIL_SIZE_X - 2; i++)
-  {
-    printf("%4.2g ",values[current_buffer][index_send[d][0] + i][index_send[d][1]]);
-  }
-  puts("");
-  
-  for (int i = 0; i < STENCIL_SIZE_X - 2; i++)
-  {
-    printf("%4.2g ", buff[i]);//values[current_buffer][index_recv[d][0] + i][index_recv[d][1]]);
-  }
-  puts("\n");*/
-
   for (int i = 0; i < LAST_DIRECTION; ++i) {
     if (i != CENTER) {
       my_send_recv_directions(&values[current_buffer][index_send[i][0]][index_send[i][1]],
@@ -258,7 +238,7 @@ void stencil_update() {
 }
 
 /** return 1 if computation has converged */
-static int stencil_test_convergence(void)
+static int stencil_test_convergence_local(void)
 {
   int prev_buffer = (current_buffer - 1 + STENCIL_NBUFFERS) % STENCIL_NBUFFERS;
   int x, y;
@@ -269,6 +249,14 @@ static int stencil_test_convergence(void)
     }
   }
   return 1;
+}
+
+static int stencil_test_convergence(void) {
+  int local_converged, global_converged;
+
+  local_converged = stencil_test_convergence_local();
+  MPI_Allreduce(&local_converged, &global_converged, 1, MPI_INT, MPI_LAND, grid);
+  return global_converged;
 }
 
 static int stencil_all_in_one(void) {
@@ -325,47 +313,25 @@ int main(int argc, char**argv)
 
   create_types();
 
-  int size = STENCIL_SIZE_X;
-  //for (int size = 10; size < MAX_STENCIL_SIZE; size *= 1.25) {
-    /* TODO : Corriger le calcul de new_stencil_* */
-    
-    //for (STENCIL_SIZE_Y = 10; STENCIL_SIZE_Y < MAX_STENCIL_SIZE_Y; STENCIL_SIZE_Y *= 1.25) {
-  printf("[%d] : %d, %d\n", grid_rank, coords[0], coords[1]);
   stencil_init();
 
-  for (int step = 0; step < 4; ++step)
+  struct timespec t1, t2;
+  clock_gettime(CLOCK_MONOTONIC, &t1);
+  for (int step = 0; step < stencil_max_steps; ++step)
   {
     stencil_step();
     stencil_update();
-    printf("\nstep %d : \n", step + 1);
-    stencil_display((step + 1)%2, 0, STENCIL_SIZE_X-1, 0, STENCIL_SIZE_Y-1);
+    if(stencil_test_convergence()) {
+      break;
+    }
   }
+  clock_gettime(CLOCK_MONOTONIC, &t2);
+  const double t_usec = (t2.tv_sec - t1.tv_sec) * 1000000.0 + (t2.tv_nsec - t1.tv_nsec) / 1000.0;
+  double avg_t_usec;
+
+  MPI_Reduce(&t_usec, &avg_t_usec, 1, MPI_DOUBLE, MPI_MAX, 0, grid);
+  if (grid_rank == 0) {printf("Temps de calcul maximal entre les processus %g (usec)\n", t_usec);}
   
-#if 0
-      struct timespec t1, t2;
-      clock_gettime(CLOCK_MONOTONIC, &t1);
-      int s;
-      for(s = 0; s < stencil_max_steps; s++) {
-#ifdef ALL_IN_ONE
-        if(stencil_all_in_one()) {
-          break;
-        }
-#else
-        stencil_step();
-        if(stencil_test_convergence())
-        {
-          break;
-        }
-#endif
-      }
-      clock_gettime(CLOCK_MONOTONIC, &t2);
-      const double t_usec = (t2.tv_sec - t1.tv_sec) * 1000000.0 + (t2.tv_nsec - t1.tv_nsec) / 1000.0;
-      printf("%d,%g,%d\n", size, t_usec, s);
-      //stencil_display(current_buffer, 0, STENCIL_SIZE_X - 1, 0, STENCIL_SIZE_Y - 1);
-      stencil_free();
-    //}
-  //}
-#endif//0
   MPI_Comm_free(&grid);
 
   free_types();
